e042d3df2288d0b3d78b644c0187b31150f331f7
Ivana Paulina Ruiz Cervantes <ivana.ruizcrv@uanl.edu.mx>
2024-10-09 08:14:41 -0600

    Nuevo archivo para manejar alrtas javascript

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log('Hola bienvenido a la página de NYC'); /* Esto es texto que se ve solo en consola, en la página no */
+console.error('Error!') /* Para marcar errores en la consola */
+alert('¡¡Por favor, antes de comenzar, reproduce la canción dandole play al reproductor para tener la experiencia completa!!'); /* Salen en la pantalla como alertas */
